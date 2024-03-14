c672497bc0d493676afbbdd71bee4012d15ce33e
zinface <zinface@163.com>
2024-03-14 09:55:16 +0800

    repo: 处理默认显示的多屏位置问题

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,24 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QCursor>
 #include <QDebug>
 #include <QDesktopWidget>
 #include <QMainWindow>
+#include <QPoint>
+#include <QScreen>
 
-void moveCenter(MainWindow &wm) {
-    QDesktopWidget *desktop = QApplication::desktop();
-    wm.move((desktop->width() - wm.width()) /2,(desktop->height() - wm.height()) /2);
+void moveCenter(QWidget &wm) {
+    QScreen *screen = QApplication::screenAt(QCursor::pos());
+    auto desktop = screen->geometry();
+    auto afterX = (desktop.width() - wm.width()) / 2;
+    auto afterY = (desktop.height() - wm.height()) /2;
+
+    wm.move(afterX + desktop.x(), afterY + desktop.y());
+
+    qDebug() << screen->availableGeometry();
+    qDebug() << screen->geometry();
+    qDebug() << QCursor::pos();
 }
 
 int main(int argc, char *argv[])
